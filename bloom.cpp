bdb593da577cb0c5c5ec083bf13e81e0cdff3606
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-06-07 17:53:35 +0200

    Bloom's tests finished

--- bloom.cpp
+++ bloom.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <set>
-#include <cassert>
-
 #include <memory>
 #include <algorithm>
 #include <cstdint>
@@ -120,20 +117,77 @@ class bloom_filter_t
 
 }
 
+#include <iostream>
+#include <set>
+#include <cassert>
+
+#define TEST_SIZE       100000
+#define TEST_ERROR_RATE 0.01
+
 int main(int argc, char *argv[])
 {
-	lvldb::bloom_filter_t bloom(10000, 0.01);
+	lvldb::bloom_filter_t bloom(TEST_SIZE, TEST_ERROR_RATE);
+	std::set<int>         set;
+	int                   false_positives = 0;
+	double                false_positives_rate;
 
-	bloom.print_debug_info();
 	bloom.insert("foo", 3);
 	bloom.insert("bar", 3);
 	bloom.insert("fur", 3);
-	bloom.print_debug_info();
 
 	assert(bloom.member("foo", 3));
 	assert(bloom.member("bar", 3));
 	assert(bloom.member("fur", 3));
-	assert(bloom.member("123", 3) == false);
+	assert(bloom.member("x", 1) == false);
+
+	bloom.print_debug_info();
+	std::cout << std::endl;
+	bloom.clear();
+
+	for (int i = 0; i < TEST_SIZE; ++i) {
+		if (bloom.member(&i, sizeof(i)))
+			++false_positives;
+
+		set.insert(i);
+		bloom.insert(&i, sizeof(i));
+		assert(bloom.member(&i, sizeof(i)));
+	}
+
+	false_positives_rate = (double) false_positives / (double) TEST_SIZE;
+	assert(fabs(false_positives_rate - TEST_ERROR_RATE) < TEST_ERROR_RATE);
+
+	std::cout << "test_size            = " << TEST_SIZE            << "\n";
+	std::cout << "test_error_rate      = " << TEST_ERROR_RATE      << "\n";
+	std::cout << "false_positives      = " << false_positives      << "\n";
+	std::cout << "false_positives_rate = " << false_positives_rate << "\n";
+	std::cout << std::endl;
+
+	set.clear();
+	bloom.clear();
+	false_positives = 0;
+
+	for (int i = 0; i < TEST_SIZE; ++i) {
+		if (i % 3 != 0 && i % 7 != 0) {
+			set.insert(i);
+			bloom.insert(&i, sizeof(i));
+			assert(bloom.member(&i, sizeof(i)));
+		}
+	}
+
+	for (int i = 0; i < TEST_SIZE; ++i) {
+		if (set.find(i) != set.end())
+			assert(bloom.member(&i, sizeof(i)));
+		else if (bloom.member(&i, sizeof(i)))
+			++false_positives;
+	}
+
+	false_positives_rate = (double) false_positives / (double) TEST_SIZE;
+	assert(fabs(false_positives_rate - TEST_ERROR_RATE) < TEST_ERROR_RATE);
+
+	std::cout << "test_size            = " << TEST_SIZE            << "\n";
+	std::cout << "test_error_rate      = " << TEST_ERROR_RATE      << "\n";
+	std::cout << "false_positives      = " << false_positives      << "\n";
+	std::cout << "false_positives_rate = " << false_positives_rate << "\n";
 
 	return 0;
 }
