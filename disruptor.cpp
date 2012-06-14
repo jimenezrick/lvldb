061cf96161c77b4daa4a7793d63c45b382c17a21
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-06-14 02:50:34 +0200

    task_t finished!

--- disruptor.cpp
+++ disruptor.cpp
@@ -1,10 +1,11 @@
 #include <memory>
-#include <thread>
 #include <atomic>
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 #include <cassert>
 
+#include <pthread.h>
 #include <unistd.h>
 
 namespace lvldb
@@ -20,7 +21,7 @@ class disruptor_t
 
 	disruptor_t(size_t size):
 		size_(size),
-		ring_(std::unique_ptr<T[]>(new T[size]))
+		ring_(std::unique_ptr<T[]>(new T[size]()))
 	{
 		// Test if size is a power of two
 		assert((size & (size - 1)) == 0);
@@ -55,13 +56,16 @@ class fence_t
 
 	enum type_t {producer, consumer};
 
-	fence_t(disruptor_t<T> &disruptor, type_t type):
+	fence_t(disruptor_t<T> &disruptor, type_t type,
+		int atomic_retries = 4, int atomic_sleep = 100):
 		seq_(0),
 		next_(0),
 		disruptor_(disruptor),
 		type_(type),
 		next_fence_(nullptr),
-		stop_(false)
+		atomic_retries_(atomic_retries),
+		atomic_sleep_(atomic_sleep),
+		stop_tasks_(false)
 	{
 		long line_size;
 
@@ -93,10 +97,12 @@ class fence_t
 		check_consistency();
 
 		while (true) {
+			int pauses = 0;
 			seq_t next = next_;
 
-			while (disruptor_.get_index(next) == disruptor_.get_index(next_fence_->seq_))
-				pause_thread();
+			while (disruptor_.get_index(next) ==
+			       disruptor_.get_index(next_fence_->seq_))
+				pause_thread(pauses);
 
 			if (!next_.compare_exchange_strong(next, next + 1))
 				continue;
@@ -112,20 +118,29 @@ class fence_t
 
 	inline void release_slot(seq_t task_seq)
 	{
+		int pauses;
 		seq_t expected;
 
 		check_consistency();
 
-		while (disruptor_.get_index(task_seq + 1) == disruptor_.get_index(next_fence_->seq_))
-			pause_thread();
+		pauses = 0;
+		while (disruptor_.get_index(task_seq + 1) ==
+		       disruptor_.get_index(next_fence_->seq_))
+			pause_thread(pauses);
 
+		pauses = 0;
 		expected = task_seq;
 		while (!seq_.compare_exchange_strong(expected, task_seq + 1)) {
 			expected = task_seq;
-			pause_thread();
+			pause_thread(pauses);
 		}
 
 		check_consistency();
+	}
+
+	void stop_tasks()
+	{
+		stop_tasks_ = true;
 	}
 
 	private:
@@ -135,17 +150,27 @@ class fence_t
 	disruptor_t<T>   &disruptor_;
 	const type_t      type_;
 	const fence_t    *next_fence_;
-	std::atomic<bool> stop_;
+	const int         atomic_retries_;
+	const int         atomic_sleep_; // Milliseconds
+	std::atomic<bool> stop_tasks_;
 
-	inline void pause_thread()
+	inline void pause_thread(int &pauses)
 	{
-		//
-		// XXX: Pausa con espera exponencial tras varias iteraciones
-		//      Crear 2 atributos: atomic_retries_, atomic_sleep_
-		//
-		//      Despes de dormir bastante, comprobar una variable global atomic por si deben finalizar los threads
-		//
-		__asm__ __volatile__("pause" ::: "memory");
+		if (pauses < atomic_retries_)
+			__asm__ __volatile__("pause" ::: "memory");
+		else {
+			struct timespec req = {0, atomic_sleep_ * 1000 * 1000};
+
+			if (stop_tasks_)
+				pthread_exit(nullptr);
+
+			if (nanosleep(&req, nullptr) == -1) {
+				perror("nanosleep");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		pauses++;
 	}
 
 	inline void check_consistency()
@@ -160,133 +185,203 @@ class fence_t
 template<typename T>
 class task_t
 {
-	//
-	// XXX: Usar pthreads
-	//
-
 	public:
 
 	task_t(fence_t<T> &fence):
 		fence_(fence)
 	{ }
 
+	static void *start_thread(void *arg)
+	{
+		static_cast<task_t *>(arg)->run();
+
+		return nullptr;
+	}
+
 	void start()
 	{
-		thread_ = std::thread(&task_t::run, this);
+		if (pthread_create(&thread_, nullptr, start_thread, this) != 0) {
+			perror("pthread_create");
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	void stop()
 	{
-		// XXX: Avisar al thread para que pare
-		thread_.join();
+		fence_.stop_tasks();
+
+		if (pthread_join(thread_, nullptr) != 0) {
+			perror("pthread_join");
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	inline void run()
 	{
-		T &slot;
+		while (true) {
+			T &slot = fence_.acquire_slot(seq_);
 
-		slot = fence_.acquire_slot(seq_);
-		process_slot(slot);
-		fence_.release_slot(seq_);
+			process_slot(slot);
+			fence_.release_slot(seq_);
+		}
 	}
 
-	virtual void process_slot(T &slot) = 0;
-
-	private:
+	protected:
 
 	seq_t       seq_;
 	fence_t<T> &fence_;
-	std::thread thread_;
+	pthread_t   thread_;
 
-	// XXX XXX XXX
-	//
-	// Seguir con el ejemplo y que cada thread modifique el slot del ring y comprobar
-	//
-	// Hacer un benchmark contra un ring con un mutex en cada slot
-	//
-	// XXX XXX XXX
+	virtual void process_slot(T &slot) = 0;
 };
 
 }
 
 #include <iostream>
 
-void fun1(lvldb::fence_t<long> *f)
+template<typename T>
+class task1_t: public lvldb::task_t<T>
 {
-	lvldb::seq_t s;
+	public:
 
-	while (true) {
-		f->acquire_slot(s);
-		sleep(1);
-		f->release_slot(s);
+	task1_t(lvldb::fence_t<T> &fence):
+		lvldb::task_t<T>(fence),
+		initialized_(false)
+	{ }
+
+	void process_slot(T &slot)
+	{
+		if (slot != 0)
+			initialized_ = true;
+
+		if (initialized_)
+			assert(slot % 4 == 3);
+		else
+			assert(slot == 0);
+
+		slot = this->seq_ * 4;
 
 		flockfile(stdout);
-		std::cout << "t1 s = " << s << std::endl;
+		std::cout << "task 1 seq = " << this->seq_;
+		std::cout << " slot = "      << slot << std::endl;
 		funlockfile(stdout);
 	}
-}
 
-void fun2(lvldb::fence_t<long> *f)
+	private:
+
+	bool initialized_;
+};
+
+template<typename T>
+class task2_t: public lvldb::task_t<T>
 {
-	lvldb::seq_t s;
+	public:
 
-	while (true) {
-		f->acquire_slot(s);
-		f->release_slot(s);
+	task2_t(lvldb::fence_t<T> &fence):
+		lvldb::task_t<T>(fence)
+	{ }
+
+	void process_slot(T &slot)
+	{
+		if (this->seq_ > 0)
+			assert(slot > 0);
+
+		assert(static_cast<size_t>(slot) == this->seq_ * 4);
+
+		slot = this->seq_ * 4 + 1;
 
 		flockfile(stdout);
-		std::cout << "t2 s = " << s << std::endl;
+		std::cout << "task 2 seq = " << this->seq_;
+		std::cout << " slot = "      << slot << std::endl;
 		funlockfile(stdout);
 	}
-}
+};
 
-void fun3(lvldb::fence_t<long> *f)
+template<typename T>
+class task3_t: public lvldb::task_t<T>
 {
-	lvldb::seq_t s;
+	public:
 
-	while (true) {
-		f->acquire_slot(s);
-		f->release_slot(s);
+	task3_t(lvldb::fence_t<T> &fence):
+		lvldb::task_t<T>(fence)
+	{ }
+
+	void process_slot(T &slot)
+	{
+		if (this->seq_ > 0)
+			assert(slot > 0);
+
+		assert(static_cast<size_t>(slot) == this->seq_ * 4);
+
+		slot = this->seq_ * 4 + 2;
 
 		flockfile(stdout);
-		std::cout << "t3 s = " << s << std::endl;
+		std::cout << "task 3 seq = " << this->seq_;
+		std::cout << " slot = "      << slot << std::endl;
 		funlockfile(stdout);
 	}
-}
+};
 
-void fun4(lvldb::fence_t<long> *f)
+template<typename T>
+class task4_t: public lvldb::task_t<T>
 {
-	lvldb::seq_t s;
+	public:
 
-	while (true) {
-		f->acquire_slot(s);
-		f->release_slot(s);
+	task4_t(lvldb::fence_t<T> &fence):
+		lvldb::task_t<T>(fence)
+	{ }
+
+	void process_slot(T &slot)
+	{
+		if (this->seq_ > 0)
+			assert(slot > 0);
+
+		assert(static_cast<size_t>(slot) == this->seq_ * 4 + 1 ||
+		       static_cast<size_t>(slot) == this->seq_ * 4 + 2);
+
+		slot = this->seq_ * 4 + 3;
 
 		flockfile(stdout);
-		std::cout << "t4 s = " << s << std::endl;
+		std::cout << "task 4 seq = " << this->seq_;
+		std::cout << " slot = "      << slot << std::endl;
 		funlockfile(stdout);
 	}
-}
+};
+
+// XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
+//
+// Hacer un benchmark contra un ring con un mutex en cada slot
+// Implementar un fence_mutex_t???
+//
+// XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
 
 int main(int argc, char *argv[])
 {
 	lvldb::disruptor_t<long> d(8);
-	lvldb::fence_t<long>     f1(d, lvldb::fence_t<long>::producer), f2(d, lvldb::fence_t<long>::consumer);
+	lvldb::fence_t<long>     f1(d, lvldb::fence_t<long>::producer);
+	lvldb::fence_t<long>     f2(d, lvldb::fence_t<long>::consumer);
 	lvldb::fence_t<long>     f3(d, lvldb::fence_t<long>::consumer);
+
+	task1_t<long> t1(f1);
+	task2_t<long> t2(f2);
+	task3_t<long> t3(f2);
+	task4_t<long> t4(f3);
 
 	f3.set_next_fence(&f2);
 	f2.set_next_fence(&f1);
 	f1.set_next_fence(&f3);
 
-	std::thread t4(&fun4, &f3);
-	std::thread t3(&fun3, &f2);
-	std::thread t2(&fun2, &f2);
-	std::thread t1(&fun1, &f1);
+	t1.start();
+	t2.start();
+	t3.start();
+	t4.start();
 
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
+	sleep(10);
+
+	t1.stop();
+	t2.stop();
+	t3.stop();
+	t4.stop();
 
 	return 0;
 }
